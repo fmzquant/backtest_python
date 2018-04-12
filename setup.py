dc5d6addd129ab5807035e79ea6caf330581b5fe
wwbustb <1051804485@qq.com>
2018-04-12 18:06:06 +0800

    setup test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='botvs',
+      version='1.0',
+      description='Botvs local backtest',
+      author='Zero',
+      author_email='hi@botvs.com',
+      url='https://www.botvs.com/',
+      packages=[""],
+     )
