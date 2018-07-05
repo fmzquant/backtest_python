bab1dbe4b19e352dd895324d22e0fd5ac09bd4f3
Zero <zeropool@qq.com>
2018-07-05 10:44:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='botvs',
+setup(name='fmz',
       version='1.0',
-      description='Botvs local backtest',
+      description='FMZ local backtest',
       author='Zero',
-      author_email='hi@botvs.com',
-      url='https://www.botvs.com/',
+      author_email='hi@fmz.com',
+      url='https://www.fmz.com/',
       packages=[""]
      )
