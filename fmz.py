c8b29e740323ef1304d60166008f97e6e018e5f9
Zero <zeropool@qq.com>
2018-12-17 16:43:23 +0800

    Update fmz.py

--- fmz.py
+++ fmz.py
@@ -959,7 +959,7 @@ class VCtx(object):
             js = os.path.join(tmpCache, 'md5.json')
             if os.path.exists(js):
                 b = open(js, 'rb').read()
-                if os.getenv("BOTVS_TASK_UUID") is None or "fa088b9c6ab629f1bdd9664a2bf2713c" in str(b):
+                if os.getenv("BOTVS_TASK_UUID") is None or "df898fcc3071e0665836568071fb965b" in str(b):
                     hdic = json_loads(b)
             loader = os.path.join(tmpCache, soName)
             update = False
