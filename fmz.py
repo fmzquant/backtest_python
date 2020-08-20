dc91ff2bb72b0f1af4899fb759eb5c153be9fb2b
Zero <zeropool@qq.com>
2020-08-21 02:30:06 +0800

    Add files via upload

--- fmz.py
+++ fmz.py
@@ -1079,7 +1079,7 @@ class VCtx(object):
             js = os.path.join(tmpCache, 'md5.json')
             if os.path.exists(js):
                 b = open(js, 'rb').read()
-                if os.getenv("BOTVS_TASK_UUID") is None or "869ac1dce3c638bb3a9fe6942df96377" in str(b):
+                if os.getenv("BOTVS_TASK_UUID") is None or "3e2fe7bfba8a03fdb9d481ad61195409" in str(b):
                     hdic = json_loads(b)
             loader = os.path.join(tmpCache, soName)
             update = False
