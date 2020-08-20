b4db09f9dfc86ee2e2c2d58df3efed882148dd8d
Zero <zeropool@qq.com>
2020-08-20 15:57:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='fmz',
-      version='1.1',
+      version='1.2',
       description='FMZ local backtest',
       author='Zero',
       author_email='hi@fmz.com',
