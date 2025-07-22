b2e9d96cae201718b95b8213e63e07c247569003
Zero <smurfszero@icloud.com>
2025-07-22 16:44:07 +0800

    Add files via upload

--- fmz.py
+++ fmz.py
@@ -1468,7 +1468,7 @@ class VCtx(object):
             js = os.path.join(tmpCache, crcFile)
             if os.path.exists(js):
                 b = open(js, 'rb').read()
-                if os.getenv("BOTVS_TASK_UUID") is None or "5561d6b943250172016187b0723215a1" in str(b):
+                if os.getenv("BOTVS_TASK_UUID") is None or "9ecf04aa8459a20183987d03afe47627" in str(b):
                     hdic = json_loads(b)
             loader = os.path.join(tmpCache, soName)
             update = False
